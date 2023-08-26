3fc37a704f4fa537aea474c7d77dc8824c62b4ef
Kelvin Zhang <github@iamkelv.in>
2023-08-27 00:26:18 +0100

    Add profile photo to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 import About from '@/content/about.mdx';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='prose prose-lg'>
-      <About />
+    <main>
+      <div className='mb-4 flex justify-center'>
+        <Image
+          src='/assets/profile.jpg'
+          alt='Photo of Kelvin Zhang'
+          width={200}
+          height={200}
+          className='rounded-full'
+        />
+      </div>
+      <div className='prose prose-lg'>
+        <About />
+      </div>
     </main>
   );
 }
