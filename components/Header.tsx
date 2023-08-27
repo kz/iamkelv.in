6c8a373cd25888cfcc765b98e3b12af899c680bc
Kelvin Zhang <github@iamkelv.in>
2023-08-27 01:22:02 +0100

    Add social icons beneath my name

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,20 @@
 import Link from 'next/link';
+import { IconType } from 'react-icons';
+import {
+  IoLogoLinkedin,
+  IoMdMail,
+  IoLogoTwitter,
+  IoLogoGithub,
+} from 'react-icons/io';
 
 interface HeaderLinkProps {
   href: string;
   text: string;
+}
+
+interface SocialIconProps {
+  Icon: IconType;
+  href: string;
 }
 
 const HeaderLink: React.FC<HeaderLinkProps> = ({ href, text }) => (
@@ -11,14 +23,31 @@ const HeaderLink: React.FC<HeaderLinkProps> = ({ href, text }) => (
   </Link>
 );
 
+const SocialIcon: React.FC<SocialIconProps> = ({ Icon, href }) => (
+  <a target='_blank' href={href} className='h-7 w-7'>
+    <Icon />
+  </a>
+);
+
 const Header = () => (
-  <header className='mb-4 flex justify-between'>
-    <div className='text-lg font-semibold'>Kelvin Zhang</div>
-    <nav className='space-x-2 md:space-x-6'>
-      <HeaderLink href='/' text='About' />
-      <HeaderLink href='/blog' text='Blog' />
-      <HeaderLink href='/projects' text='Projects' />
-    </nav>
+  <header>
+    <div className='flex justify-between'>
+      <div className='text-lg font-semibold'>Kelvin Zhang</div>
+      <nav className='space-x-3 md:space-x-6'>
+        <HeaderLink href='/' text='About' />
+        <HeaderLink href='/blog' text='Blog' />
+        <HeaderLink href='/projects' text='Projects' />
+      </nav>
+    </div>
+    <div className='mb-4 flex'>
+      <SocialIcon
+        Icon={IoLogoLinkedin}
+        href='https://linkedin.com/in/kelvzhan'
+      />
+      <SocialIcon Icon={IoLogoTwitter} href='https://twitter.com/kelvzhan' />
+      <SocialIcon Icon={IoMdMail} href='mailto:hello@iamkelv.in' />
+      <SocialIcon Icon={IoLogoGithub} href='https://github.com/kz' />
+    </div>
   </header>
 );
 
