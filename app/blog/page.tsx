1ca1ffb25aab0aba8c5c82eb9c11a8f25a13f60c
Kelvin Zhang <github@iamkelv.in>
2023-08-28 21:15:06 +0100

    Add external links to blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,43 @@
-import { getPostsMetadata } from '@/lib/blog';
+import { BlogMetadata, getBlogLinksMetadata } from '@/lib/blog';
 import dayjs from 'dayjs';
 import Link from 'next/link';
+import { IoMdOpen } from 'react-icons/io';
 
 function toDateString(date: Date) {
   return dayjs(date).format('MMM YYYY');
 }
 
-export default async function Page() {
-  const posts = await getPostsMetadata();
+const BlogLink = (metadata: BlogMetadata) => {
+  if (metadata.type === 'external') {
+    return (
+      // flex is needed to get the icon on the same line as the title.
+      <a
+        href={metadata.link}
+        target='_blank'
+        className='text-lg text-blue-600 hover:underline'
+      >
+        {metadata.title}{' '}
+        <span className='inline-block text-gray-400'>
+          <IoMdOpen size='16' />
+        </span>
+      </a>
+    );
+  } else {
+    return (
+      <Link
+        href={`/blog/${metadata.slug}`}
+        className='text-lg text-blue-600 hover:underline'
+      >
+        {metadata.title}
+      </Link>
+    );
+  }
+};
 
-  posts
+export default async function Page() {
+  const blogLinks = await getBlogLinksMetadata();
+
+  blogLinks
     .map((post) => post)
     .sort((a, b) => {
       if (a.created > b.created) return 1;
@@ -21,17 +49,12 @@ export default async function Page() {
   return (
     <section>
       <ul>
-        {posts.map((post) => {
+        {blogLinks.map((blogLink) => {
           return (
-            <li key={post.slug} className='mb-2 md:flex md:justify-between'>
-              <Link
-                href={`/blog/${post.slug}`}
-                className='text-lg text-blue-600'
-              >
-                {post.title}
-              </Link>
+            <li key={blogLink.slug} className='mb-2 md:flex md:justify-between'>
+              {BlogLink(blogLink)}
               <div className='text-md whitespace-nowrap font-normal text-gray-500'>
-                {toDateString(post.created)}
+                {toDateString(blogLink.created)}
               </div>
             </li>
           );
--- lib/blog.tsx
+++ lib/blog.tsx
@@ -6,6 +6,7 @@ import matter from 'gray-matter';
 const postsPath = path.join(process.cwd(), 'content/blog');
 
 export interface PostMetadata {
+  type: 'internal';
   title: string;
   slug: string;
   created: Date;
@@ -13,22 +14,44 @@ export interface PostMetadata {
   excerpt: string;
 }
 
-function matterToPostMetadata(data: any): PostMetadata {
-  return {
-    title: data.title,
-    slug: data.slug,
-    created: data.created,
-    updated: data.updated,
-    excerpt: data.excerpt,
-  };
+export interface ExternalMetadata {
+  type: 'external';
+  title: string;
+  // Slug is used as a unique identifier.
+  slug: string;
+  link: string;
+  created: Date;
 }
 
-export async function getPostsMetadata() {
+export type BlogMetadata = PostMetadata | ExternalMetadata;
+
+function matterToMetadata(data: any): BlogMetadata {
+  if (data.type === 'external') {
+    return {
+      type: 'external',
+      title: data.title,
+      slug: data.slug,
+      link: data.link,
+      created: data.created,
+    };
+  } else {
+    return {
+      type: 'internal',
+      title: data.title,
+      slug: data.slug,
+      created: data.created,
+      updated: data.updated,
+      excerpt: data.excerpt,
+    };
+  }
+}
+
+export async function getBlogLinksMetadata() {
   const paths = sync(`${postsPath}/*.mdx`);
 
   return paths.map((path) => {
     const source = fs.readFileSync(path);
     const { data } = matter(source);
-    return matterToPostMetadata(data);
+    return matterToMetadata(data);
   });
 }
