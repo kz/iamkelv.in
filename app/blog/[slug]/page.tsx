6181eff8bf0f96bab8eaffc2dee06682d8d823dd
Kelvin Zhang <github@iamkelv.in>
2023-09-12 21:39:04 +0100

    Fix heading anchor classes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -14,24 +14,30 @@ export async function generateStaticParams() {
 export default async function Page({ params }: { params: { slug: string } }) {
   const { metadata, content } = await getBlogPostBySlug(params.slug);
 
-  const inlineCodeStyles = [
-    'rounded-lg',
-    'border',
-    'border-solid',
-    'border-slate-300',
-    'bg-slate-100',
-    'px-2',
-    'py-1',
-    'text-sm',
-    'font-normal',
-    'before:content-none',
-    'after:content-none',
-  ]
-    .map((style) => `[&_:not(pre)>code]:${style}`)
-    .join(' ');
+  const inlineCodeClasses = [
+    '[&_:not(pre)>code]:rounded-lg',
+    '[&_:not(pre)>code]:border',
+    '[&_:not(pre)>code]:border-solid',
+    '[&_:not(pre)>code]:border-slate-300',
+    '[&_:not(pre)>code]:bg-slate-100',
+    '[&_:not(pre)>code]:px-2',
+    '[&_:not(pre)>code]:py-1',
+    '[&_:not(pre)>code]:text-sm',
+    '[&_:not(pre)>code]:font-normal',
+    '[&_:not(pre)>code]:before:content-none',
+    '[&_:not(pre)>code]:after:content-none',
+  ].join(' ');
+
+  const headingAnchorClasses = [
+    '[&_:is(h1,h2,h3,h4,h5,h6,th)>a]:font-bold',
+    '[&_:is(h1,h2,h3,h4,h5,h6,th)>a]:no-underline',
+    'hover:[&_:is(h1,h2,h3,h4,h5,h6,th)>a]:underline',
+  ].join(' ');
 
   return (
-    <div className={`prose prose-lg ${inlineCodeStyles}`}>
+    <div
+      className={`prose prose-lg ${inlineCodeClasses} ${headingAnchorClasses}`}
+    >
       <Markdown content={content} />
     </div>
   );
